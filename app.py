d77c875939a005036c75a026488c5b81b824d734
mariananresende <148554023+mariananresende@users.noreply.github.com>
2025-04-09 08:05:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def vector_embedding():
     pdf_paths = [
         "Manual_de_Uso_Documenta_Wiki_MDS_SAGICAD.pdf",
         "Roteiro_video_divulgacao.pdf",
-        "Roteiro_video_tutorial_edicao.pdf",
+        "Roteiro_Tutorial_Documenta_Wiki.pdf",
         "Ficha de Indicador.pdf",
         "Ficha de Programa.pdf",
         "Protocolo_nomeacao_indicadores.pdf"
